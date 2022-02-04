865675b5f10c5a5d346d5189591396c82f993204
MeesPos <meespostma_@hotmail.com>
2022-02-04 14:18:09 +0100

    Added create new article button

--- resources/js/Pages/Dashboard/Articles.vue
+++ resources/js/Pages/Dashboard/Articles.vue
@@ -11,6 +11,18 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+                        <div class="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap">
+                            <div class="ml-4 mt-2">
+                                <h3 class="text-lg leading-6 font-medium text-gray-900">All Articles</h3>
+                            </div>
+                            <div class="ml-4 mt-2 flex-shrink-0">
+                                <Link as="button" type="button" :href="route('articles.create')" class="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    Create new article
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
                     <div class="flex flex-col">
                         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
